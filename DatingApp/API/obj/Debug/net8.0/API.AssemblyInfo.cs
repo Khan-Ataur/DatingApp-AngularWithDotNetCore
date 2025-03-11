003e683958882b089c8d18ab98c2211ef90b9228
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-12 00:36:08 +0600

    create User Register DTO for API end point (S4,V34)
    User RegisterDto model added to save user registrations

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b21a86b5bb6e944d399bccf512cbf1f1f54eb91b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48643f0dbfc3d6f78ef40fe9a40c985b39c14bc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
