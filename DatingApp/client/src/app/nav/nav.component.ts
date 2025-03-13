6cd1b2238366bd1912446b64e00a0223b9977dab
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-13 21:04:24 +0600

    Using conditionals (*ngIf) to show and remove content (S5,V49)
    how to use IF conditions in angular html/ template file based on login / logout

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
@@ -19,12 +20,14 @@ export class NavComponent {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn=true;
+        this.loggedIn = true;
       },
-      error:error=> console.log(error)
+      error: error => console.log(error)
     })
-
-
-
   }
+
+  logout() {
+    this.loggedIn = false;
+  }
+
 }
