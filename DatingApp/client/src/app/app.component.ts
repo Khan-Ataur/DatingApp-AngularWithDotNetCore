07b612fbfe9ccc105953eea7dec26ca9126e0030
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-14 01:29:05 +0600

    ** Storing User Login token using Angular Signals and for use (S5,V52)
    storing user login token using Signals

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,32 +1,61 @@
 
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
- 
+
 
   title = 'DatingApp';
-  
   users: any;
+  private accountService = inject(AccountService);
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) {}
+  /*ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response: any) => this.users = response,
+      error: (error: any) => console.log(error),
+      complete: () => console.log('Request has completed.')
+    });
+  } */
+
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString)
+    {
+      return;
+    }
+
+    const userLoginInfo = JSON.parse(userString);
+    this.accountService.currentUser.set(userLoginInfo);
+
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: (response: any) => this.users = response,
       error: (error: any) => console.log(error),
       complete: () => console.log('Request has completed.')
     });
   }
+
+
+
 
 }
