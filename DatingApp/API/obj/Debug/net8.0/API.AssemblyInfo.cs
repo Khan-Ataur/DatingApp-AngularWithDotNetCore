21b8c72e0ad27a32a11e47e67a4e21e2cba7ad83
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-16 23:06:29 +0600

    user register from client done. (S5,V57)

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1833faab4bdb5352f1df12382bd04943648287")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+621ce270692aadb75ce68c20ad98fe48e9bd57a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -8,15 +9,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  usersFromHomeComponent= input.required<any>(); 
-  // here getting data from home.component.ts as --> home(parent) is using register(child) components; there is a child parent relationship between two components
-  
+ private accountService = inject(AccountService); 
   cancelRegister = output<boolean>(); // now passing Child(register) to parent(Home) value when user press Cancel button
   
   model:any={}
 
   register(){
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: response =>{
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+      
+    })
   }
 
   cancel()
