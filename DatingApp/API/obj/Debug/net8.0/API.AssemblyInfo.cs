7e44b78bc64b8f33659dcfc9ea6542d59d21dfb5
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-18 00:41:25 +0600

    routing in TS code file (S6,V62)

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc28a2d75e350d24d66f9e43b9ba0dd08696245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc06ed6cc540d6b33e0e54eb41219ae68b0c339")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
