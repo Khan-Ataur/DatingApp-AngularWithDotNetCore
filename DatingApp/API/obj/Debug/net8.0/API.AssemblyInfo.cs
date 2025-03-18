3fcb1c7ec9c0d65f951c961454c007b6ac68a0cd
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-18 23:38:57 +0600

    Route Auth and nav bar show when only login (S6,V65)

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d63c129dd2a5479c430b8d775aa04bade7f5b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd373987abcffc37a2c945527347a1cdc01c772")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
