691dbfdbed5bb528c284ee76f0279ee3124bd8d7
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-19 00:00:33 +0600

    Adding a new free bootswatch theme (S6,V66)
    ## Implement a bootstrap free theme in angular application [https://bootswatch.com/ ]
    install: >> npm i bootswatch
    now add Theme css to angular.json file
    => "node_modules/bootswatch/dist/united/bootstrap.min.css"

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd373987abcffc37a2c945527347a1cdc01c772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fcb1c7ec9c0d65f951c961454c007b6ac68a0cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
