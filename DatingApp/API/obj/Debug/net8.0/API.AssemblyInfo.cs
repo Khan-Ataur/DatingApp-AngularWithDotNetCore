a32df16279785144b531d690fa691d0730e4103d
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-20 00:20:32 +0600

    create Error Handler Middleware (S7,V71)
    Error Handler Middleware
    > First need to add a class :    ApiException
    > Custom middleware class create : ExceptionMiddleware
    > use this custom Middleware to Progam.cs file like app.UseMiddleware<ExceptionMiddleware>();

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691dbfdbed5bb528c284ee76f0279ee3124bd8d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b7a5acd3659dc9962e3e3011954adee856422f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
