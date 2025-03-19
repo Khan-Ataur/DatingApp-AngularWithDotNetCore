f7b7a5acd3659dc9962e3e3011954adee856422f
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-19 23:21:35 +0600

    Creating an error controller for testing errors (S7,V69)
    BuggyController

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fcb1c7ec9c0d65f951c961454c007b6ac68a0cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691dbfdbed5bb528c284ee76f0279ee3124bd8d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
