e75ed65064f6263dc77b1d53b0989aa2556e36b8
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-20 21:55:28 +0600

    client angular project add error interceptor (S7,V73)

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32df16279785144b531d690fa691d0730e4103d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720eee7330a9ef193e69bbf9f47ac4214ea187be")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
