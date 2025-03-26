96e430c4f6df4992aed221962a42b4dbbd96f106
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-03-27 00:30:24 +0600

    mistakenly  .cs files not added. now added all .cs

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf706369de2b26db554317ec359350e42952af15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ff4df5955d1c98260ad585886868de34dec696")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
