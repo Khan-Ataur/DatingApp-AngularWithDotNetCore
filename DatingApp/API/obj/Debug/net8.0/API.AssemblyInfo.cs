03f679d17e4afe0d801e2c23f670dd8445459842
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-05-04 22:39:11 +0600

    User Profile info edit component add and design (S10,V110)

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32dfd6732555bd1ecfd2a258b9418dc6134e45d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006459ada9cd51d5042d645a9eb7c7dee5f84a16")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
