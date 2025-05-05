7fc5bca9fe002980bda703d5574b2e03f4b8acd2
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2025-05-05 21:39:08 +0600

    How to update/submit angular form (S10,V111)

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006459ada9cd51d5042d645a9eb7c7dee5f84a16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f679d17e4afe0d801e2c23f670dd8445459842")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
